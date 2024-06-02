13ca600dcb0b203c7c9f6799be1ee0f034dbf571
Przemysław Bulanda <devprzemas@gmail.com>
2024-06-03 01:46:06 +0200

    Add basic files and create background

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ParticlesComponent from "./style/Background/particles";
+import { Route, Routes } from "react-router-dom";
+const App = () => {
+  return (
+    <>
+      <ParticlesComponent />
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
